30b93542599b08794c6bd6c19881d8b5c4a63dc4
weslleymirandadev <contato@weslleymiranda.dev>
2025-12-06 11:36:55 -0300

    feat(prisma): added client & schema

---
+++ src/lib/prisma.ts
@@ -0,0 +1,11 @@
+import { PrismaClient } from '@/generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
+
+const connectionString = process.env.DATABASE_URL;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+
+let prisma = new PrismaClient({ adapter });
+
+export default prisma;
