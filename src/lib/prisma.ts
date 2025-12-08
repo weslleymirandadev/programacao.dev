1e265c814395d94579279c4d4364d2cc41c949d9
weslleymirandadev <contato@weslleymiranda.dev>
2025-12-08 19:40:41 -0300

    fix(dev): removed queries

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -8,7 +8,7 @@ export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
     accelerateUrl: process.env.DATABASE_URL!,
-    log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+    log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
   });
 
 if (process.env.NODE_ENV === "development") {
